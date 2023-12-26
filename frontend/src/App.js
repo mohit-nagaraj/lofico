f84d15a7f9d9d641883c9e661aaa735a8544b13e
mohit-nagaraj <mohitnagaraj20@gmail.com>
2023-12-26 08:51:38 +0530

    Add analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Analytics />
     </div>
   );
 }
