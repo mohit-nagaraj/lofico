e2758db219f32708f3693505228b4641f79f9825
mohit-nagaraj <mohitnagaraj20@gmail.com>
2025-01-10 19:18:49 +0530

    few stuff

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import userRouter from './routes/userRoute';
 
 const app = express();
-const port = process.env.PORT;
+dotenv.config();
+const port = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 app.get('/health', (req, res) => {
     res.status(200).send('OK');
 });
+
+app.use("/api/users", userRouter);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
